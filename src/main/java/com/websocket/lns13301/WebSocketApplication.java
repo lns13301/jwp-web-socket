4068e85dcf1f972ea4b31fd8f8a4650df758887e
lns13301 <rkdrbgks22@ks.ac.kr>
2021-11-08 14:06:07 +0900

    feat: 웹 소켓 Sample 추가

--- src/main/java/com/websocket/lns13301/WebSocketApplication.java
+++ src/main/java/com/websocket/lns13301/WebSocketApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lns13301Application {
+public class WebSocketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lns13301Application.class, args);
+		SpringApplication.run(WebSocketApplication.class, args);
 	}
 
 }
