3d52900285d0c4ae06184274ddbcb39158c1da2e
lns13301 <rkdrbgks22@ks.ac.kr>
2021-11-08 13:49:21 +0900

    feat: Initial commit.

---
+++ src/main/java/com/websocket/lns13301/Lns13301Application.java
@@ -0,0 +1,13 @@
+package com.websocket.lns13301;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lns13301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lns13301Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/websocket/lns13301/Lns13301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.lns13301;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lns13301ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
